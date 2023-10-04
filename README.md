32bd8e1ce54b74734a895bb62483cb4653a5541a
Angelina <lina.shalimanova@list.ru>
2023-10-04 15:56:47 +0300

    Updating project

--- README.md
+++ README.md
@@ -3,11 +3,37 @@
 ## Цель разработки: 
 Заинтересовать людей в путешествиях по России.
 ## Команда проекта
-- Анастасия Стрелкова — Front-End Engineer
-- Ангелина Шалиманова — works with documentation
-- Валерий Пецинярж — 
+- Анастасия Стрелкова — Front-End Engineer, Back-End Engineer
+- Ангелина Шалиманова — Back-End Engineer, works with documentation
+- Валерий Пецинярж — Back-End Engineer
 ## Зависимости проекта
-+ telebot
-+ pandas
-+ lxml
++ certifi==2022.12.7
++ charset-normalizer==3.1.0
++ idna==3.4
++ lxml==4.9.2
++ numpy==1.24.3
++ pandas==2.0.1
++ pyTelegramBotAPI==4.11.0
++ python-dateutil==2.8.2
++ pytz==2023.3
++ requests==2.28.2
++ six==1.16.0
++ telebot==0.0.5
++ tzdata==2023.3
++ urllib3==1.26.15
+## Команда для установки проекта
+`pip install dist/Telegram_bot-0.0.1.tar.gz`
+
 ## Команда для запуска проекта
+```
+py -m venv venv
+venv\Scripts\activate
+py -m pip install -r requirements.txt
+pipenv run python main.py
+```
+
+## Команда для генерации дистрибутива
+```
+pip install -q build
+python -m build
+```
--- main.py
+++ main.py
@@ -10,12 +10,23 @@ bot = telebot.TeleBot('6107680735:AAH9rrQINqK0IDzmxnlBVgVRtn5w5MsSFNw')
 # Обработка кнопки /start
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'Привет, я Russian Trip Bot! Я умею искать интересные места в городах России, которые ты мне напишешь 🗺')
+    """
+    Вывод начального сообщения по нажатию кнопки /start
+    :param message:
+    :return: None
+    """
+    bot.send_message(message.chat.id, 'Привет, я Russian Trip Bot! Я умею искать интересные места '
+                                      'в городах России, которые ты мне напишешь 🗺')
     bot.send_message(message.chat.id, 'Чтобы начать, напиши мне название города России, который хочешь посетить.')
 
 
 # Получение данных из базы данных
 def get_db(city):
+    """
+    Получение информации о городе из базы данных
+    :param city: название введённого города
+    :return: data: Информация о городе из баззы данных
+    """
     connect = sqlite3.connect('db.db')
     cursor = connect.cursor()
     query = 'SELECT * FROM trip WHERE city = ?'
@@ -27,10 +38,19 @@ def get_db(city):
 
 # Разделение и вывод данных
 def split_db(message, data):
+    """
+    Форматированный вывод данных
+    :param message:
+    :param data: Информация о городе из базы данных
+    :return:
+    """
     for i in data:
         text = f'<b>{i[2]}</b>\n\n{i[3]}\n\n<i>{i[4]}</i>'
         photo = i[1].split('; ')
-        media = [telebot.types.InputMediaPhoto(open(f'{photo[0]}', 'rb'), caption=text, parse_mode='html'), telebot.types.InputMediaPhoto(open(f'{photo[1]}', 'rb')), telebot.types.InputMediaPhoto(open(f'{photo[2]}', 'rb'))]
+        media = [telebot.types.InputMediaPhoto(open(f'{photo[0]}', 'rb'),
+                                               caption=text, parse_mode='html'),
+                 telebot.types.InputMediaPhoto(open(f'{photo[1]}', 'rb')),
+                 telebot.types.InputMediaPhoto(open(f'{photo[2]}', 'rb'))]
         bot.send_media_group(message.chat.id, media)
         link = i[5].split('; ')
         markup = types.InlineKeyboardMarkup()
@@ -58,17 +78,24 @@ def split_db(message, data):
 # Выводит 5 мест (3 картинки, описание, адрес и кнопку с отзывами в Яндексе + сайт (если есть))
 @bot.message_handler()
 def info(message):
+    """
+    Управление выводом информации
+    :param message:
+    :return: None
+    """
     city = message.text
-    data = get_db(city)  # вся инфа из бд
+    data = get_db(city)  # вся информация из бд
     url = 'https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D0%B3%D0%BE%D1%80%D0%BE%D0%B4%D0%BE%D0%B2_%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D0%B8'
     df = pd.read_html(url)[0]
     if city in df.values:
         if not data:
-            bot.send_message(message.chat.id, 'Такого города пока нет в моем списке 😔 Но в скором времени мы это исправим 😉')
+            bot.send_message(message.chat.id, 'Такого города пока нет в моем '
+                                              'списке 😔 Но в скором времени мы это исправим 😉')
         else:
             bot.send_message(message.chat.id, f'Вот список интересных мест города <u>{city}</u>:', parse_mode='html')
             split_db(message, data)
     else:
         bot.send_message(message.chat.id, 'Введите корректное название города 🫣')
+
 
 bot.polling(none_stop=True)
